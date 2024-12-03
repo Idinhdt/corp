73b31809a5eaf3dfffcca9d801daa12b4f4433a6
Idinhdt <ida.johanssonnienhardt@elev.ga.ntig.se>
2024-12-03 17:48:31 +0100

    3/12
    kommit igång med layout där mman ska kunna skrolla och välja ut saker, samt fixat så jag ska kunna ha flera charts men som är byggda samma

--- js/charts.js
+++ js/charts.js
@@ -1,27 +1,83 @@
-const ctx1 = document.getElementById('myChart');
+const allastats = ['Movement', 'Toughness', 'Save', 'Wounds', 'Leadership', 'Objective control']
 
-new Chart(ctx1, {
- type: 'bar',
- data: {
-   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-   datasets: [{
-     label: '# of Votes',
-     data: [12, 19, 3, 5, 2, 3],
-     borderWidth: 1
-   }]
- }                      
-});
+function Mall(ctx, data) {
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: allastats,
+      datasets: [{
+        label: 'Stat',
+        data: data,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      animation: false,
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false 
+        }
+      },
+      scales: {
+        x: {
+          grid: {
+            display: false
+          },
+          ticks: {
+            maxRotation: 0,
+            minRotation: 0,
+          }
+        },
+        y: {
+          grid: {
+            display: false
+          }
+        }
+      },
+      layout: {
+        padding: {
+          top: 20
+        }
+      }
+    },
+    plugins: [{
+      id: 'nummer',
+      afterDatasetsDraw(chart) {
+        const { ctx, data, scales: { x, y } } = chart;
 
-const ctx2 = document.getElementById('myCart');
+        data.datasets.forEach((dataset, datasetIndex) => {
+          const meta = chart.getDatasetMeta(datasetIndex);
 
-new Chart(ctx2, {
- type: 'bar',
- data: {
-   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-   datasets: [{
-     label: '# of Votes',
-     data: [12, 9, 3, 5, 2, 3],
-     borderWidth: 1
-   }]
- }                      
-});
+          meta.data.forEach((bar, index) => {
+            const value = dataset.data[index];
+
+            const suffixes = ['"', '', '+', '', '+', ''];
+            const formattedValue = `${value}${suffixes[index]}`;
+
+            ctx.save();
+            ctx.font = '12px Arial';
+            ctx.fillStyle = 'black';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
+            ctx.fillText(formattedValue, bar.x, bar.y - 5);
+            ctx.restore();
+          });
+        });
+      }
+    }]
+  });
+}
+
+Chart.defaults.borderColor = 'rgba (0, 0, 0, 1)';
+Chart.defaults.backgroundColor = 'rgba (0, 0, 0, 1)';
+
+const MD1 = document.getElementById('DG1');
+const data1 = [5, 5, 3, 4, 6, 1]; 
+
+const MD2 = document.getElementById('DG2');
+const data2 = [4, 6, 2, 5, 7, 2]; 
+
+
+Mall(MD1, data1);
+Mall(MD2, data2);
---
+++ js/popup.js
@@ -0,0 +1,19 @@
+
+  var modal = document.getElementById("d1");
+  var img = document.getElementById("dm1");
+  var span = document.getElementsByClassName("close")[0];
+
+  
+  img.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
